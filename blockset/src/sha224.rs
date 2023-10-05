cc87116a0d7df8844aceb91a8aa1cc71dfd87807
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2023-10-05 16:54:02 -0700

    cosmetic

--- blockset/src/sha224.rs
+++ blockset/src/sha224.rs
@@ -79,22 +79,23 @@ const fn wi(w: &U512, i: usize) -> u128 {
     w[i & 3]
 }
 
-//   0123|4567|89AB|CDEF
+//   0   |1   |2   |3
 //   0123|0123|0123|0123
+//   0123|4567|89AB|CDEF
 // 0:WR  |    | R  |  R
 // 1: WR |    |  R |   R
 // 2:R WR|    |   R|
 // 3: R W|R   |    |R
 const fn w_round4(w: &U512, i: usize) -> u128 {
+    let [mut w00, mut w01, mut w02, mut w03] = to_u32x4(w[i]);
     let w10 = wi(w, i + 1) as u32;
     let [_, w21, w22, w23] = to_u32x4(wi(w, i + 2));
     let [w30, _, w32, w33] = to_u32x4(wi(w, i + 3));
-    let mut w0 = to_u32x4(w[i]);
-    w0[0] = w_round(w0[0], w0[1], w21, w32);
-    w0[1] = w_round(w0[1], w0[2], w22, w33);
-    w0[2] = w_round(w0[2], w0[3], w23, w0[0]);
-    w0[3] = w_round(w0[3], w10, w30, w0[1]);
-    to_u128(w0)
+    w00 = w_round(w00, w01, w21, w32);
+    w01 = w_round(w01, w02, w22, w33);
+    w02 = w_round(w02, w03, w23, w00);
+    w03 = w_round(w03, w10, w30, w01);
+    to_u128([w00, w01, w02, w03])
 }
 
 const fn w_round16(mut w: U512) -> U512 {
