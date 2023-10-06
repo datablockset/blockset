d2926b9511c73703cd8591ae9435e9a18f571e6a
sergey-shandar <sergey-shandar@users.noreply.github.com>
2023-10-06 13:39:20 -0700

    more shl tests

--- blockset/src/digest.rs
+++ blockset/src/digest.rs
@@ -10,15 +10,22 @@ const fn len(&[_, b]: &U256) -> usize {
     if result == 0xFF { LEN_MAX + 1 } else { result >> 3 }
 }
 
+const DATA_MASK: u128 = (1 << 120) - 1;
+
+const fn remove_len(&[a, b]: &U256) -> U256 {
+    [a, b & DATA_MASK]
+}
+
 /*
-const fn merge(a: &U256, b: &U256) -> U256 {
-    let a_len = len(a);
-    let b_len = len(b);
+const fn merge(mut a: U256, mut b: U256) -> U256 {
+    let a_len = len(&a);
+    let b_len = len(&b);
     let len = a_len + b_len;
     if len <= LEN_MAX {
-
+        a = remove_len(&a);
+        b = remove_len(&b);
     } else {
 
-    }
+    };
 }
 */
