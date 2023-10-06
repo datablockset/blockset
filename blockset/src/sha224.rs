1991652b4f2d337c25a4f931f6ee011770cb8dd9
sergey-shandar <sergey-shandar@users.noreply.github.com>
2023-10-06 13:16:42 -0700

    shl

--- blockset/src/sha224.rs
+++ blockset/src/sha224.rs
@@ -1,10 +1,10 @@
 use crate::{
-    u32::{add, add3, add4},
     sigma32::{BIG0, BIG1, SMALL0, SMALL1},
     u128::{from_u32x4, get_u32, to_u32x4},
     u224::U224,
     u256::{to_u32x8, u32x8_add, U256},
-    u512::U512,
+    u32::{add, add3, add4},
+    u512::{get_u128, new, U512},
 };
 
 const fn round([s0, s1]: U256, i: usize, w: u128, k: u128) -> U256 {
@@ -27,8 +27,8 @@ const fn round([s0, s1]: U256, i: usize, w: u128, k: u128) -> U256 {
 }
 
 const fn round4(mut x: U256, i: usize, w: &U512, k: &U512) -> U256 {
-    let w = w[i];
-    let k = k[i];
+    let w = get_u128(w, i);
+    let k = get_u128(k, i);
     x = round(x, 0, w, k);
     x = round(x, 1, w, k);
     x = round(x, 2, w, k);
@@ -36,30 +36,30 @@ const fn round4(mut x: U256, i: usize, w: &U512, k: &U512) -> U256 {
 }
 
 pub const K: [U512; 4] = [
-    [
+    new(
         0xe9b5dba5_b5c0fbcf_71374491_428a2f98,
         0xab1c5ed5_923f82a4_59f111f1_3956c25b,
         0x550c7dc3_243185be_12835b01_d807aa98,
         0xc19bf174_9bdc06a7_80deb1fe_72be5d74,
-    ],
-    [
+    ),
+    new(
         0x240ca1cc_0fc19dc6_efbe4786_e49b69c1,
         0x76f988da_5cb0a9dc_4a7484aa_2de92c6f,
         0xbf597fc7_b00327c8_a831c66d_983e5152,
         0x14292967_06ca6351_d5a79147_c6e00bf3,
-    ],
-    [
+    ),
+    new(
         0x53380d13_4d2c6dfc_2e1b2138_27b70a85,
         0x92722c85_81c2c92e_766a0abb_650a7354,
         0xc76c51a3_c24b8b70_a81a664b_a2bfe8a1,
         0x106aa070_f40e3585_d6990624_d192e819,
-    ],
-    [
+    ),
+    new(
         0x34b0bcb5_2748774c_1e376c08_19a4c116,
         0x682e6ff3_5b9cca4f_4ed8aa4a_391c0cb3,
         0x8cc70208_84c87814_78a5636f_748f82ee,
         0xc67178f2_bef9a3f7_a4506ceb_90befffa,
-    ],
+    ),
 ];
 
 const fn round16(mut x: U256, w: &U512, i: usize) -> U256 {
@@ -77,7 +77,7 @@ const fn w_round(w0: u32, w1: u32, w9: u32, we: u32) -> u32 {
 
 #[inline(always)]
 const fn wi(w: &U512, i: usize) -> u128 {
-    w[i & 3]
+    get_u128(w, i)
 }
 
 // ```
@@ -94,7 +94,7 @@ const fn wi(w: &U512, i: usize) -> u128 {
 // - `R` - read
 // - `W` - read and write.
 const fn w_round4(w: &U512, i: usize) -> u128 {
-    let [mut w00, mut w01, mut w02, mut w03] = to_u32x4(w[i]);
+    let [mut w00, mut w01, mut w02, mut w03] = to_u32x4(wi(w, i));
     let w10 = wi(w, i + 1) as u32;
     let [_, w21, w22, w23] = to_u32x4(wi(w, i + 2));
     let [w30, _, w32, w33] = to_u32x4(wi(w, i + 3));
@@ -106,10 +106,10 @@ const fn w_round4(w: &U512, i: usize) -> u128 {
 }
 
 const fn w_round16(mut w: U512) -> U512 {
-    w[0] = w_round4(&w, 0);
-    w[1] = w_round4(&w, 1);
-    w[2] = w_round4(&w, 2);
-    w[3] = w_round4(&w, 3);
+    w[0][0] = w_round4(&w, 0);
+    w[0][1] = w_round4(&w, 1);
+    w[1][0] = w_round4(&w, 2);
+    w[1][1] = w_round4(&w, 3);
     w
 }
 
@@ -134,11 +134,11 @@ pub const fn compress(mut w: U512) -> U224 {
 
 #[cfg(test)]
 mod test {
-    use crate::u224::U224;
+    use crate::{u224::U224, u512::new};
 
     use super::compress;
 
-    const A: U224 = compress([0x8000_0000, 0, 0, 0]);
+    const A: U224 = compress(new(0x8000_0000, 0, 0, 0));
 
     #[test]
     fn test() {
@@ -151,7 +151,7 @@ mod test {
     #[test]
     fn runtime_test() {
         assert_eq!(
-            compress([0x8000_0000, 0, 0, 0]),
+            compress(new(0x8000_0000, 0, 0, 0)),
             [0xd14a028c, 0x2a3a2bc9, 0x476102bb, 0x288234c4, 0x15a2b01f, 0x828ea62a, 0xc5b3e42f]
         );
     }
--- blockset/src/u512.rs
+++ blockset/src/u512.rs
@@ -1,1 +1,11 @@
-pub type U512 = [u128; 4];
+use crate::u256::U256;
+
+pub type U512 = [U256; 2];
+
+pub const fn new(a: u128, b: u128, c: u128, d: u128) -> U512 {
+    [[a, b], [c, d]]
+}
+
+pub const fn get_u128(a: &U512, i: usize) -> u128 {
+    a[(i >> 1) & 1][i & 1]
+}
