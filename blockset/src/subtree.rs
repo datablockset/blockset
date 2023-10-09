3e3bf709d63ce1a246dac54275dd9576113b9562
sergey-shandar <sergey-shandar@users.noreply.github.com>
2023-10-09 09:13:15 -0700

    f5

--- blockset/src/subtree.rs
+++ blockset/src/subtree.rs
@@ -124,19 +124,28 @@ mod test {
 
     #[test]
     fn subtree2_test() {
-        let ff = to_digest(0b1111_1111);
-        let fe = to_digest(0b1111_1110);
-        let fd = to_digest(0b1111_1101);
-        let fc = to_digest(0b1111_1100);
-        let fb = to_digest(0b1111_1011);
+        let ff = to_digest(0b1111_1111); // 000
+        let fe = to_digest(0b1111_1110); // 255 000
+        let fd = to_digest(0b1111_1101); // 254
+        let fc = to_digest(0b1111_1100); // 255 254 000
+        let fb = to_digest(0b1111_1011); // 253
+        let fa = to_digest(0b1111_1010); // 255 253
+        let f9 = to_digest(0b1111_1001); // 254
+        let f8 = to_digest(0b1111_1000); // 255 254 253 000
+        let f7 = to_digest(0b1111_0111); // 252
+        let f6 = to_digest(0b1111_0110); // 255 252
+        let f5 = to_digest(0b1111_0100); // 254
         let mut t = SubTree::default();
         assert_eq!(t.push(&ff), None);
         assert_eq!(t.0, [Node::new(&ff, &ff, 0)]);
+        //
         assert_eq!(t.push(&fe), None);
         assert_eq!(t.0, [Node::new(&ff, &ff, 0), Node::new(&fe, &fe, 255)]);
+        //
         assert_eq!(t.push(&fd), None);
         let ff_fe = merge(&ff, &fe);
         assert_eq!(t.0, [Node::new(&ff_fe, &fe, 0), Node::new(&fd, &fd, 254)]);
+        //
         assert_eq!(t.push(&fc), None);
         assert_eq!(
             t.0,
@@ -146,12 +155,70 @@ mod test {
                 Node::new(&fc, &fc, 255)
             ]
         );
+        //
         assert_eq!(t.push(&fb), None);
+        let ff_fc = merge(&ff_fe, &merge(&fd, &fc));
         assert_eq!(
             t.0,
             [
-                Node::new(&merge(&ff_fe, &merge(&fd, &fc)), &fc, 0),
+                Node::new(&&ff_fc, &fc, 0),
                 Node::new(&fb, &fb, 253),
+            ]
+        );
+        //
+        assert_eq!(t.push(&fa), None);
+        assert_eq!(t.0,
+            [
+                Node::new(&&ff_fc, &fc, 0),
+                Node::new(&fb, &fb, 253),
+                Node::new(&fa, &fa, 255),
+            ]
+        );
+        //
+        assert_eq!(t.push(&f9), None);
+        let fb_fa = merge(&fb, &fa);
+        assert_eq!(t.0,
+            [
+                Node::new(&&ff_fc, &fc, 0),
+                Node::new(&fb_fa, &fa, 253),
+                Node::new(&f9, &f9, 254),
+            ]
+        );
+        //
+        assert_eq!(t.push(&f8), None);
+        assert_eq!(t.0,
+            [
+                Node::new(&&ff_fc, &fc, 0),
+                Node::new(&fb_fa, &fa, 253),
+                Node::new(&f9, &f9, 254),
+                Node::new(&f8, &f8, 255),
+            ]
+        );
+        //
+        assert_eq!(t.push(&f7), None);
+        let ff_f8 = merge(&ff_fc, &merge(&fb_fa, &merge(&f9, &f8)));
+        assert_eq!(t.0,
+            [
+                Node::new(&ff_f8, &f8, 0),
+                Node::new(&f7, &f7, 252),
+            ]
+        );
+        //
+        assert_eq!(t.push(&f6), None);
+        assert_eq!(t.0,
+            [
+                Node::new(&ff_f8, &f8, 0),
+                Node::new(&f7, &f7, 252),
+                Node::new(&f6, &f6, 255),
+            ]
+        );
+        //
+        assert_eq!(t.push(&f5), None);
+        assert_eq!(t.0,
+            [
+                Node::new(&ff_f8, &f8, 0),
+                Node::new(&merge(&f7, &f6), &f6, 252),
+                Node::new(&f5, &f5, 254),
             ]
         );
     }
