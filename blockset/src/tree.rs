9c92e41ccc9dceef284ae2e9f84ea9af503b00f3
sergey-shandar <sergey-shandar@users.noreply.github.com>
2023-10-09 17:20:19 -0700

    it works!?

--- blockset/src/tree.rs
+++ blockset/src/tree.rs
@@ -39,6 +39,8 @@ impl Tree {
 
 #[cfg(test)]
 mod test {
+    use crate::{u256::U256, merge};
+
     use super::Tree;
 
     #[test]
@@ -48,8 +50,83 @@ mod test {
     }
 
     #[test]
-    fn hello_world() {
+    fn hello_world_test() {
+        //  48656c6c6f2c20776f726c6421
+        // "H e l l o , _ w o r l d ! "
         let x = Tree::push_all("Hello, world!");
-        // println!("{:x?}", x);
+        // println!("x: {:x?}", x);
+        let e = [
+            0x00000021_646c726f_77202c6f_6c6c6548,
+            0x68000000_00000000_00000000_00000000,
+        ];
+        assert_eq!(x, e);
+    }
+
+    #[test]
+    fn content_dependent_hash_tree() {
+        //  436f6e74656e742d446570656e64656e7420486173682054726565
+        //  0       1       2       3       4       5       6
+        // "C o n t e n t - D e p e n d e n t _ H a s h _ T r e e "
+        let x = Tree::push_all("Content-Dependent Hash Tree");
+        let e: U256 = [
+            0x6e65646e_65706544_2d746e65_746e6f43,
+            0xD8000000_00656572_54206873_61482074,
+        ];
+        println!("x: {:x?}", x);
+        println!("e: {:x?}", e);
+        assert_eq!(x, e);
+    }
+
+    #[test]
+    fn text_test() {
+        // len: 1ab
+        // 496d6167696e6520696e74657263657074696e67206d657373616765732066726f6d206578747261746572726573747269616c732e
+        // 0       1       2       3       4       5       6       7       8       9       A       B       C       D
+        // I m a g i n e _ i n t e r c e p t i n g _ m e s s a g e s _ f r o m _ e x t r a t e r r e s t r i a l s .
+        //                                   |                                     |
+        let v = "Imagine intercepting messages from extraterrestrials.";
+        // - 0x24 (120)
+        //   - 0x11
+        //     - 0x6
+        //       - "Im"
+        //       - "ag"
+        //       - "in"
+        //     - 0xB
+        //       - "e i"
+        //       - "nt"
+        //       - "er"
+        //       - "ce"
+        //       - "pt"
+        //   - 0x13
+        //     - 0x5
+        //       - "in"
+        //       - "g m"
+        //     - 0x5
+        //       - "es"
+        //       - "sag"
+        //     - 0x9
+        //       - "es"
+        //       - " f"
+        //       - "rom e"
+        // - 0x11
+        //   - 0xA
+        //     - "xtrat"
+        //     - "er"
+        //     - "res"
+        //   - 0x7
+        //     - "trial"
+        //     - "s."
+
+        // a = "Imagine intercept"
+        //               p e c r  e t n i  _ e n i  g a m I                                      t
+        let a: U256 = [0x70656372_65746e69_20656e69_67616d49, 0x88000000_00000000_00000000_00000074];
+        // b = "ing messages from e"
+        //               o r f  _ s e  g a  s s e m  _ g n i                                e  _ m
+        let b: U256 = [0x6f7266_2073656761_7373656d_20676e69, 0x98000000_00000000_00000000_0065206d];
+        // c = "xtraterrestrials."
+        //               s l a i  r t s e  r r e t  a r t x                                      .
+        let c: U256 = [0x736c6169_72747365_72726574_61727478, 0x88000000_00000000_00000000_0000002e];
+        let x = Tree::push_all(v);
+        assert_eq!(x, merge(&merge(&a, &b), &c));
     }
 }
