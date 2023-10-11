fec8574938182bb095c6656a6e99bb7f90b880c4
sergey-shandar <sergey-shandar@users.noreply.github.com>
2023-10-11 00:05:18 -0700

    full path

--- .cloudflare/cloudflare.sh
+++ .cloudflare/cloudflare.sh
@@ -1,3 +1,2 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-source "$HOME/.cargo/env"
-cargo doc --document-private-items
+$HOME/.cargo/bin/cargo doc --document-private-items
