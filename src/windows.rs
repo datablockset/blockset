cb3d5b54bc18245f502217db63e08bc656b9747e
sergey-shandar <sergey-shandar@users.noreply.github.com>
2023-10-24 06:49:04 -0700

    Windows with OperationResult

--- src/windows.rs
+++ src/windows.rs
@@ -2,11 +2,14 @@
 #![cfg(not(tarpaulin_include))]
 use std::{ffi::CStr, io, os::windows::raw::HANDLE, ptr::null_mut};
 
-use crate::windows_api::{
-    to_bool, CancelIoEx, CloseHandle, CreateFileA, CreationDisposition, GetLastError,
-    GetOverlappedResult, ReadFile, WriteFile, ACCESS_MASK, BOOL, CREATE_ALWAYS, DWORD,
-    ERROR_IO_PENDING, FILE_FLAG_OVERLAPPED, GENERIC_READ, GENERIC_WRITE, INVALID_HANDLE_VALUE,
-    LPCVOID, LPVOID, OPEN_ALWAYS, OVERLAPPED,
+use crate::{
+    io::{AsyncFile, AsyncIo, AsyncOperation, OperationResult},
+    windows_api::{
+        to_bool, CancelIoEx, CloseHandle, CreateFileA, CreationDisposition, GetLastError,
+        GetOverlappedResult, ReadFile, WriteFile, ACCESS_MASK, BOOL, CREATE_ALWAYS, DWORD,
+        ERROR_IO_PENDING, FILE_FLAG_OVERLAPPED, GENERIC_READ, GENERIC_WRITE, INVALID_HANDLE_VALUE,
+        LPCVOID, LPVOID, OPEN_ALWAYS, OVERLAPPED,
+    },
 };
 
 struct Handle(HANDLE);
@@ -15,6 +18,19 @@ impl Drop for Handle {
     fn drop(&mut self) {
         unsafe {
             CloseHandle(self.0);
+        }
+    }
+}
+
+fn to_operation_result(v: BOOL) -> OperationResult {
+    if to_bool(v) {
+        OperationResult::Ok(0)
+    } else {
+        let e = unsafe { GetLastError() };
+        if e == ERROR_IO_PENDING {
+            OperationResult::Pending
+        } else {
+            OperationResult::Err(e.to_error())
         }
     }
 }
@@ -37,8 +53,6 @@ impl Handle {
             )
         };
         if result == INVALID_HANDLE_VALUE {
-            let e = io::Error::last_os_error();
-            println!("{}", e);
             Err(io::Error::last_os_error())
         } else {
             Ok(Self(result))
@@ -56,21 +70,13 @@ impl Handle {
         overlapped: &'a mut Overlapped,
         result: BOOL,
     ) -> io::Result<Operation<'a>> {
-        if result.into() {
-            return Ok(Operation {
+        if let OperationResult::Err(e) = to_operation_result(result) {
+            Err(e)
+        } else {
+            Ok(Operation {
                 handle: self,
                 overlapped,
-            });
-        } else {
-            let e = unsafe { GetLastError() };
-            if e == ERROR_IO_PENDING {
-                Ok(Operation {
-                    handle: self,
-                    overlapped,
-                })
-            } else {
-                Err(e.to_error())
-            }
+            })
         }
     }
 
@@ -129,7 +135,7 @@ impl Drop for Operation<'_> {
 }
 
 impl Operation<'_> {
-    fn get_result(&mut self, wait: bool) -> io::Result<usize> {
+    fn get_result(&mut self, wait: bool) -> OperationResult {
         let mut result: DWORD = 0;
         let r = unsafe {
             GetOverlappedResult(
@@ -140,28 +146,95 @@ impl Operation<'_> {
             )
         };
         if r.into() {
-            Ok(result as usize)
+            OperationResult::Ok(result as usize)
         } else {
-            Err(io::Error::last_os_error())
+            let e = unsafe { GetLastError() };
+            if e == ERROR_IO_PENDING {
+                OperationResult::Pending
+            } else {
+                OperationResult::Err(e.to_error())
+            }
         }
+    }
+}
+
+impl AsyncOperation for Operation<'_> {
+    fn get_result(&mut self) -> OperationResult {
+        self.get_result(false)
+    }
+}
+
+struct AFile {
+    file: Handle,
+    overlapped: Overlapped,
+}
+
+impl AsyncFile for AFile {
+    type Operation<'a> = Operation<'a>;
+
+    fn read<'a>(&'a mut self, buffer: &'a mut [u8]) -> io::Result<Self::Operation<'a>> {
+        self.file.read(&mut self.overlapped, buffer)
+    }
+
+    fn write<'a>(&'a mut self, buffer: &'a [u8]) -> io::Result<Self::Operation<'a>> {
+        self.file.write(&mut self.overlapped, buffer)
+    }
+}
+
+struct AIo();
+
+impl AsyncIo for AIo {
+    type File = AFile;
+
+    fn create(&self, path: &CStr) -> io::Result<Self::File> {
+        Ok(AFile {
+            file: Handle::create(path)?,
+            overlapped: Default::default(),
+        })
+    }
+
+    fn open(&self, path: &CStr) -> io::Result<Self::File> {
+        Ok(AFile {
+            file: Handle::open(path)?,
+            overlapped: Default::default(),
+        })
     }
 }
 
 #[cfg(test)]
 mod test {
-    use std::ffi::CString;
+    use std::{ffi::CString, thread::yield_now};
+
+    use crate::io::OperationResult;
 
     #[test]
     fn test() {
         use super::{Handle, Overlapped};
         //
         let x: CString = CString::new("_test.txt").unwrap();
+        let origin = b"Hello World!";
         {
             let mut handle = Handle::create(&x).unwrap();
             let mut overlapped = Overlapped::default();
-            let mut operation = handle.write(&mut overlapped, b"Hello World!").unwrap();
-            let result = operation.get_result(true).unwrap();
-            assert_eq!(result, 12);
+            let mut operation = handle.write(&mut overlapped, origin).unwrap();
+            loop {
+                match operation.get_result(false) {
+                    OperationResult::Ok(bytes_written) => {
+                        if bytes_written != origin.len() {
+                            panic!();
+                        }
+                        break;
+                    }
+                    OperationResult::Pending => {
+                        yield_now();
+                    }
+                    OperationResult::Err(e) => {
+                        panic!("e: {}", e);
+                    }
+                }
+            }
+            // let result = operation.get_result(true).unwrap();
+            // assert_eq!(result, 12);
         }
         {
             let mut handle = Handle::open(&x).unwrap();
@@ -169,8 +242,24 @@ mod test {
             let mut buffer = [0u8; 1024];
             {
                 let mut operation = handle.read(&mut overlapped, &mut buffer).unwrap();
-                let result = operation.get_result(true).unwrap();
-                assert_eq!(result, 12);
+                loop {
+                    match operation.get_result(false) {
+                        OperationResult::Ok(bytes_written) => {
+                            if bytes_written != origin.len() {
+                                panic!();
+                            }
+                            break;
+                        }
+                        OperationResult::Pending => {
+                            yield_now();
+                        }
+                        OperationResult::Err(e) => {
+                            panic!("e: {}", e);
+                        }
+                    }
+                }
+                // let result = operation.get_result(true).unwrap();
+                // assert_eq!(result, 12);
             }
             assert_eq!(&buffer[..12], b"Hello World!");
         }
