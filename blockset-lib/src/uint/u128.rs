057fce7bc130448875c251aa0f1a908b194898a8
sergey-shandar <sergey-shandar@users.noreply.github.com>
2023-11-10 12:07:54 -0800

    comments

--- blockset-lib/src/uint/u128.rs
+++ blockset-lib/src/uint/u128.rs
@@ -1,5 +1,8 @@
 use crate::uint::u32::add;
 
+/// Converts a 128-bit unsigned integer (`u128`) into a vector of four 32-bit unsigned integers (`[u32; 4]`).
+/// This function essentially 'splits' the 128-bit value into a vector of four components, each representing a
+/// 32-bit segment, starting from the least significant bits.
 #[inline(always)]
 pub const fn to_u32x4(v: u128) -> [u32; 4] {
     [
@@ -10,16 +13,22 @@ pub const fn to_u32x4(v: u128) -> [u32; 4] {
     ]
 }
 
+/// Reconstructs a 128-bit unsigned integer (`u128`) from a vector of four 32-bit unsigned integers (`[u32; 4]`).
+/// This operation is the inverse of `to_u32x4`, combining the vector components back into a single 128-bit value.
 #[inline(always)]
 pub const fn from_u32x4([w0, w1, w2, w3]: [u32; 4]) -> u128 {
     w0 as u128 | ((w1 as u128) << 32) | ((w2 as u128) << 64) | ((w3 as u128) << 96)
 }
 
+/// Extracts a single 32-bit component (element) from a 128-bit vector (`u128`) at a specified index.
+/// The index `i` determines which 32-bit segment to extract, with `0` being the least significant.
 #[inline(always)]
 pub const fn get_u32(v: u128, i: usize) -> u32 {
     (v >> (i << 5)) as u32
 }
 
+/// Performs element-wise addition of two 128-bit vectors (`u128`), represented as arrays of 32-bit components.
+/// Each component of the vectors is added using `add`, which handles overflow by wrapping around.
 #[inline(always)]
 pub const fn u32x4_add(a: u128, b: u128) -> u128 {
     let [a0, a1, a2, a3] = to_u32x4(a);
--- blockset-lib/src/uint/u32.rs
+++ blockset-lib/src/uint/u32.rs
@@ -14,21 +14,30 @@ pub const fn add3(a: u32, b: u32, c: u32) -> u32 {
     add(add(a, b), c)
 }
 
+/// Adds four `u32` integers and returns the sum.
+///
+/// This function safely handles integer overflow without panicking.
 #[inline(always)]
 pub const fn add4(a: u32, b: u32, c: u32, d: u32) -> u32 {
     add(add(a, b), add(c, d))
 }
 
+/// Adds five `u32` integers and returns the sum.
+///
+/// This function safely handles integer overflow without panicking.
 #[inline(always)]
 pub const fn add5(a: u32, b: u32, c: u32, d: u32, e: u32) -> u32 {
     add3(add3(a, b, c), d, e)
 }
 
+/// Converts a `u32` integer into an array of four `u8` bytes (little-endian).
 #[inline(always)]
 pub const fn to_u8x4(a: u32) -> [u8; 4] {
-    [a as u8, (a >> 8) as u8, (a >> 16) as u8, (a >> 24) as u8]
+    // [a as u8, (a >> 8) as u8, (a >> 16) as u8, (a >> 24) as u8]
+    a.to_le_bytes()
 }
 
+/// Constructs a `u32` integer from an array of four `u8` bytes (little-endian).
 #[inline(always)]
 pub const fn from_u8x4(a: &[u8; 4]) -> u32 {
     a[0] as u32 | ((a[1] as u32) << 8) | ((a[2] as u32) << 16) | ((a[3] as u32) << 24)
