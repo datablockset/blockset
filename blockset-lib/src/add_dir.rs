05cd0ee45a33575004600f9e418af033a80fba45
sergey-shandar <sergey-shandar@users.noreply.github.com>
2024-03-05 11:43:02 -0800

    a/b.txt

--- blockset-lib/src/add_dir.rs
+++ blockset-lib/src/add_dir.rs
@@ -1,6 +1,6 @@
 use std::io;
 
-use io_trait::Io;
+use io_trait::{Io, DirEntry};
 use nanvm_lib::{js::new::New, mem::global::GLOBAL, serializer::WriteJson};
 
 use crate::{app::invalid_input, common::print::Print};
@@ -8,8 +8,10 @@ use crate::{app::invalid_input, common::print::Print};
 pub fn add_dir<T: Io>(io: &T, mut a: T::Args) -> io::Result<()> {
     let path = a.next().ok_or(invalid_input("missing directory name"))?;
     let list = io.read_dir(path.as_str())?;
-    let a = GLOBAL.new_js_array(
-        list.into_iter().map(|_| GLOBAL.new_js_string([])));
+    let a = GLOBAL.new_js_array(list.into_iter().map(|s| {
+        let s16 = s.path().encode_utf16().collect::<Vec<_>>();
+        GLOBAL.new_js_string(s16)
+    }));
     let mut s = String::default();
     s.write_json(a)
         .map_err(|_| invalid_input("write to JSON"))?;
@@ -30,10 +32,13 @@ mod test {
     fn test() {
         let io = VirtualIo::new(&["a"]);
         io.create_dir("a").unwrap();
+        io.create("a/b.txt").unwrap();
+        io.create("c.txt").unwrap();
+        // io.create("a/c.txt").unwrap();
         let mut a = io.args();
         a.next().unwrap();
         add_dir(&io, a).unwrap();
         let result = io.stdout.to_stdout();
-        assert_eq!(result, "add-dir: []\n");
+        assert_eq!(result, "add-dir: [\"a/b.txt\"]\n");
     }
 }
