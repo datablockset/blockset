77ab5e4e371f5894624873186c4a6fbe5ce02d34
sergey-shandar <sergey-shandar@users.noreply.github.com>
2024-03-05 14:50:26 -0800

    dir

--- blockset-lib/src/app/add_dir.rs
+++ blockset-lib/src/app/add_dir.rs
@@ -1,6 +1,6 @@
 use std::io;
 
-use io_trait::{DirEntry, Io};
+use io_trait::{DirEntry, Io, Metadata};
 use nanvm_lib::{js::new::New, mem::global::GLOBAL, serializer::to_json};
 
 use crate::{app::invalid_input, common::print::Print};
@@ -9,8 +9,12 @@ pub fn add_dir<T: Io>(io: &T, mut a: T::Args) -> io::Result<()> {
     let path = a.next().ok_or(invalid_input("missing directory name"))?;
     let list = io.read_dir(path.as_str())?;
     let a = GLOBAL.new_js_array(list.into_iter().map(|s| {
-        let s16 = s.path().encode_utf16().collect::<Vec<_>>();
-        GLOBAL.new_js_string(s16)
+        if s.metadata().unwrap().is_dir() {
+            GLOBAL.new_js_string([])
+        } else {
+            let s16 = s.path().encode_utf16().collect::<Vec<_>>();
+            GLOBAL.new_js_string(s16)
+        }
     }));
     let list = to_json(a).map_err(|_| invalid_input("to_json"))?;
     io.stdout().println(["add-dir: ", list.as_str()])
@@ -32,10 +36,12 @@ mod test {
         io.create("a/b.txt").unwrap();
         io.create("c.txt").unwrap();
         io.create("a/d.txt").unwrap();
+        io.create_dir("a/e").unwrap();
+        io.create("a/e/f.txt").unwrap();
         let mut a = io.args();
         a.next().unwrap();
         add_dir(&io, a).unwrap();
         let result = io.stdout.to_stdout();
-        assert_eq!(result, "add-dir: [\"a/b.txt\",\"a/d.txt\"]\n");
+        assert_eq!(result, "add-dir: [\"a/b.txt\",\"a/d.txt\",\"\"]\n");
     }
 }
