87e46372665e14c99447724fc0568bdc39895a59
sergey-shandar <sergey-shandar@users.noreply.github.com>
2024-03-05 10:54:40 -0800

    fmt

--- blockset-lib/src/add_dir.rs
+++ blockset-lib/src/add_dir.rs
@@ -10,7 +10,25 @@ pub fn add_dir<T: Io>(io: &T, mut a: T::Args) -> io::Result<()> {
     let list = io.read_dir(path.as_str())?;
     let a = GLOBAL.new_js_array([]);
     let mut s = String::default();
-    s.write_json(a).map_err(|_| invalid_input("write to JSON"))?;
+    s.write_json(a)
+        .map_err(|_| invalid_input("write to JSON"))?;
     io.stdout().println(["add-dir: ", s.as_str()])?;
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use io_test::VirtualIo;
+    use io_trait::Io;
+    use wasm_bindgen_test::wasm_bindgen_test;
+
+    use super::add_dir;
+
+    #[test]
+    #[wasm_bindgen_test]
+    fn test() {
+        let io = VirtualIo::new(&[]);
+        let x = io.args();
+        add_dir(&io, x).unwrap();
+    }
 }
