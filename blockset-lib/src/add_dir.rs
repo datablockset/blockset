a844222ad73c00f196c3ba721b4f8be05419bf63
sergey-shandar <sergey-shandar@users.noreply.github.com>
2024-03-05 13:02:13 -0800

    add_dir

--- blockset-lib/src/add_dir.rs
+++ blockset-lib/src/add_dir.rs
@@ -1,9 +1,20 @@
 use std::io;
 
-use io_trait::{Io, DirEntry};
-use nanvm_lib::{js::new::New, mem::global::GLOBAL, serializer::WriteJson};
+use io_trait::{DirEntry, Io};
+use nanvm_lib::{
+    js::{any::Any, new::New},
+    mem::{global::GLOBAL, manager::Dealloc},
+    serializer::WriteJson,
+};
 
 use crate::{app::invalid_input, common::print::Print};
+
+fn to_json(a: Any<impl Dealloc>) -> io::Result<String> {
+    let mut s = String::default();
+    s.write_json(a)
+        .map_err(|_| invalid_input("write to JSON"))?;
+    Ok(s)
+}
 
 pub fn add_dir<T: Io>(io: &T, mut a: T::Args) -> io::Result<()> {
     let path = a.next().ok_or(invalid_input("missing directory name"))?;
@@ -12,11 +23,7 @@ pub fn add_dir<T: Io>(io: &T, mut a: T::Args) -> io::Result<()> {
         let s16 = s.path().encode_utf16().collect::<Vec<_>>();
         GLOBAL.new_js_string(s16)
     }));
-    let mut s = String::default();
-    s.write_json(a)
-        .map_err(|_| invalid_input("write to JSON"))?;
-    io.stdout().println(["add-dir: ", s.as_str()])?;
-    Ok(())
+    io.stdout().println(["add-dir: ", to_json(a)?.as_str()])
 }
 
 #[cfg(test)]
@@ -34,11 +41,11 @@ mod test {
         io.create_dir("a").unwrap();
         io.create("a/b.txt").unwrap();
         io.create("c.txt").unwrap();
-        // io.create("a/c.txt").unwrap();
+        io.create("a/d.txt").unwrap();
         let mut a = io.args();
         a.next().unwrap();
         add_dir(&io, a).unwrap();
         let result = io.stdout.to_stdout();
-        assert_eq!(result, "add-dir: [\"a/b.txt\"]\n");
+        assert_eq!(result, "add-dir: [\"a/b.txt\",\"a/d.txt\"]\n");
     }
 }
