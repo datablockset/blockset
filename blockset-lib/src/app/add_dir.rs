587438f914cb3eb04523704729172ba993c9d0b2
sergey-shandar <sergey-shandar@users.noreply.github.com>
2024-03-07 11:23:01 -0800

    coverage

--- blockset-lib/src/app/add_dir.rs
+++ blockset-lib/src/app/add_dir.rs
@@ -4,7 +4,10 @@ use io_trait::{DirEntry, Io, Metadata};
 use nanvm_lib::{
     common::{cast::Cast, default::default},
     js::{any_cast::AnyCast, js_object::Property, js_string::new_string, new::New},
-    mem::{global::GLOBAL, manager::Manager},
+    mem::{
+        global::GLOBAL,
+        manager::{Dealloc, Manager},
+    },
     serializer::to_json,
 };
 
@@ -15,7 +18,7 @@ fn read_dir_recursive<I: Io>(io: &I, path: &str) -> io::Result<Vec<I::DirEntry>>
     let mut dirs = [path.to_owned()].cast();
     while let Some(dir) = dirs.pop() {
         for entry in io.read_dir(dir.as_str())? {
-            if entry.metadata().unwrap().is_dir() {
+            if entry.metadata()?.is_dir() {
                 dirs.push(entry.path().to_owned());
             } else {
                 result.push(entry);
@@ -34,9 +37,23 @@ fn property<M: Manager>(m: M, path_len: usize, e: impl DirEntry) -> Property<M::
     (new_string(m, path).to_ref(), len.move_to_any())
 }
 
+fn dir_to_json<M: Manager>(
+    m: M,
+    list: impl ExactSizeIterator<Item = Property<M::Dealloc>>,
+) -> io::Result<String> {
+    to_json(m.new_js_object(list)).map_err(|_| invalid_input("to_json"))
+}
+
+fn path_to_json(io: &impl Io, path: &str) -> io::Result<String> {
+    dir_to_json(
+        GLOBAL,
+        read_dir_recursive(io, path)?
+            .into_iter()
+            .map(|s| property(GLOBAL, path.len(), s)),
+    )
+}
+
 pub fn add_dir<T: Io>(io: &T, path: &str) -> io::Result<()> {
-    let list = read_dir_recursive(io, path)?;
-    let list = list.into_iter().map(|s| property(GLOBAL, path.len(), s));
-    let list = to_json(GLOBAL.new_js_object(list)).map_err(|_| invalid_input("to_json"));
-    io.stdout().println(["add-dir: ", list?.as_str()])
+    io.stdout()
+        .println(["add-dir: ", path_to_json(io, path)?.as_str()])
 }
