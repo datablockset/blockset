43ee8ed7f55fe7da5aacd68412ebeddf934c649c
sergey-shandar <sergey-shandar@users.noreply.github.com>
2024-03-07 11:05:19 -0800

    list

--- blockset-lib/src/app/add_dir.rs
+++ blockset-lib/src/app/add_dir.rs
@@ -2,7 +2,7 @@ use std::io;
 
 use io_trait::{DirEntry, Io, Metadata};
 use nanvm_lib::{
-    common::cast::Cast,
+    common::{cast::Cast, default::default},
     js::{any_cast::AnyCast, js_object::Property, js_string::new_string, new::New},
     mem::{global::GLOBAL, manager::Manager},
     serializer::to_json,
@@ -10,18 +10,19 @@ use nanvm_lib::{
 
 use crate::{app::invalid_input, common::print::Print};
 
-fn read_dir_recursive<I: Io>(io: &I, path: &str) -> Vec<I::DirEntry> {
-    io.read_dir(path)
-        .unwrap()
-        .into_iter()
-        .flat_map(|s| {
-            if s.metadata().unwrap().is_dir() {
-                read_dir_recursive(io, s.path().as_str())
+fn read_dir_recursive<I: Io>(io: &I, path: &str) -> io::Result<Vec<I::DirEntry>> {
+    let mut result: Vec<_> = default();
+    let mut dirs = [path.to_owned()].cast();
+    while let Some(dir) = dirs.pop() {
+        for entry in io.read_dir(dir.as_str())? {
+            if entry.metadata().unwrap().is_dir() {
+                dirs.push(entry.path().to_owned());
             } else {
-                [s].cast()
+                result.push(entry);
             }
-        })
-        .collect()
+        }
+    }
+    Ok(result)
 }
 
 fn property<M: Manager>(m: M, path_len: usize, e: impl DirEntry) -> Property<M::Dealloc> {
@@ -34,7 +35,7 @@ fn property<M: Manager>(m: M, path_len: usize, e: impl DirEntry) -> Property<M::
 }
 
 pub fn add_dir<T: Io>(io: &T, path: &str) -> io::Result<()> {
-    let list = read_dir_recursive(io, path);
+    let list = read_dir_recursive(io, path)?;
     let list = list.into_iter().map(|s| property(GLOBAL, path.len(), s));
     let list = to_json(GLOBAL.new_js_object(list)).map_err(|_| invalid_input("to_json"));
     io.stdout().println(["add-dir: ", list?.as_str()])
