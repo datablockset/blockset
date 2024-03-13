7385ab86f7214dfc789780369f862393ef360e03
sergey-shandar <sergey-shandar@users.noreply.github.com>
2024-03-13 08:19:53 -0700

    coverage

--- blockset-lib/src/app/get.rs
+++ blockset-lib/src/app/get.rs
@@ -4,7 +4,7 @@ use io_trait::Io;
 use nanvm_lib::{
     js::any::Any,
     mem::manager::Manager,
-    parser::{parse_with_tokens, Context},
+    parser::{parse_with_tokens, Context, ParseError, ParseResult},
     tokenizer::tokenize,
 };
 
@@ -14,18 +14,25 @@ use crate::{
     uint::u224::U224,
 };
 
+use super::invalid_input;
+
 pub fn restore(io: &impl Io, hash: &U224, w: &mut impl Write) -> io::Result<()> {
     FileForest(io).restore(&ForestNodeId::new(NodeType::Root, hash), w, io)
 }
 
-pub fn parse_json<M: Manager>(io: &impl Io, manager: M, v: Vec<u8>) -> io::Result<Any<M::Dealloc>> {
-    let s = String::from_utf8(v)
-        .map_err(|_| io::Error::new(io::ErrorKind::InvalidData, "Invalid UTF-8"))?;
-    let tokens = tokenize(s);
-    let result = parse_with_tokens(
+fn tokenize_and_parse<M: Manager>(
+    io: &impl Io,
+    manager: M,
+    s: String,
+) -> Result<ParseResult<M>, ParseError> {
+    parse_with_tokens(
         &Context::new(manager, io, String::default()),
-        tokens.into_iter(),
-    );
-    let result = result.map_err(|_| io::Error::new(io::ErrorKind::InvalidData, "Invalid JSON"))?;
+        tokenize(s).into_iter(),
+    )
+}
+
+pub fn parse_json<M: Manager>(io: &impl Io, manager: M, v: Vec<u8>) -> io::Result<Any<M::Dealloc>> {
+    let s = String::from_utf8(v).map_err(|_| invalid_input("Invalid UTF-8"))?;
+    let result = tokenize_and_parse(io, manager, s).map_err(|_| invalid_input("Invalid JSON"))?;
     Ok(result.any)
 }
